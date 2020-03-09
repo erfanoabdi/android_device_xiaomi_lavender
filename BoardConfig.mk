0a3f87a6b1d92def58e05221b6a4c5de5446bc69
Erfan Abdi <erfangplus@gmail.com>
2020-03-09 17:32:23 +0330

    lavender: Add some TWRP configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,6 +49,7 @@ TARGET_NO_BOOTLOADER := true
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_FBE := true
 TARGET_CRYPTFS_HW_PATH := vendor/qcom/opensource/commonsys/cryptfs_hw
 TARGET_HW_DISK_ENCRYPTION := true
 
@@ -100,13 +101,14 @@ RECOVERY_SDCARD_ON_DATA := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_EXCLUDE_SUPERSU := true
 TW_EXTRA_LANGUAGES := true
-TW_DEFAULT_LANGUAGE := en
 TW_INCLUDE_NTFS_3G := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_MAX_BRIGHTNESS := 4095
 TW_DEFAULT_BRIGHTNESS := 1950
 TW_THEME := portrait_hdpi
+TW_USE_TOOLBOX := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
